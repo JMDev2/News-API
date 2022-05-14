9e2b8ee6d14be93b153bcb971a5baf7b54be0e66
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-14 16:12:27 +0300

    This will add the department Dao

--- src/main/java/interfaces/DepartmentDao.java
+++ src/main/java/interfaces/DepartmentDao.java
@@ -1,4 +1,20 @@
 package interfaces;
 
-public class DepartmentDao {
+import models.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    //    CREATE
+    void addDepartment(Department department);
+
+    //    READ
+    List<Department> getAllDepartments();
+    Department getDepartmentById(int id);
+
+
+    //    UPDATE
+//    DELETE
+    void deleteDepartment(int id);
+
 }
