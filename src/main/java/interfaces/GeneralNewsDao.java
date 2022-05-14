b4ae6921e5395dcf5ac9dbb6884ca39affb5df67
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-14 16:19:01 +0300

    This will add the general news interface

--- src/main/java/interfaces/GeneralNewsDao.java
+++ src/main/java/interfaces/GeneralNewsDao.java
@@ -1,4 +1,20 @@
 package interfaces;
 
-public class GeneralNewsDao {
+import models.GeneralNews;
+
+import java.util.List;
+
+public interface GeneralNewsDao {
+    //Create
+    void addNews(GeneralNews generalNews);
+
+    //Read
+    List<GeneralNews> getAllNews();
+    GeneralNews getGeneralNewsById(int id);
+
+    //Update
+
+    //Delete
+    void deleteGeneralNews(int id);
 }
+
