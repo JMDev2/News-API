54416b50b863049615a78d010a5c54756c731ecb
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-14 16:16:31 +0300

    This will add the department news interface with its methods

--- src/main/java/interfaces/DepartmentNewsDao.java
+++ src/main/java/interfaces/DepartmentNewsDao.java
@@ -1,4 +1,19 @@
 package interfaces;
 
-public class DepartmentNewsDao {
+import models.DepartmentNews;
+
+import java.util.List;
+
+public interface DepartmentNewsDao {
+    //Create
+    void addNews(DepartmentNews departmentNews);
+
+    //Read
+    List<DepartmentNews> getAllNews();
+    DepartmentNews getDepartmentNewsById(int id);
+
+    //Update
+
+    //Delete
+    void deleteDepartmentNews(int id);
 }
