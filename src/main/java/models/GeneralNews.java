21b2308b22e4c5865c4339ee1d7a4e828216e5d3
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-14 15:35:54 +0300

    This will add the general news class

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,71 @@
 package models;
 
+import java.util.Objects;
+
 public class GeneralNews {
+    private int id;
+    private String title;
+    private String content;
+    private int departmentId;
+
+    public GeneralNews(int id, String title, String content, int departmentId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.departmentId = departmentId;
+    }
+
+    public GeneralNews(String title, String content, int departmentId) {
+        this.title = title;
+        this.content = content;
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeneralNews that = (GeneralNews) o;
+        return id == that.id &&
+                departmentId == that.departmentId &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, departmentId);
+    }
 }
